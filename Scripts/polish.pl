9f3a5cef65e3fb12e8db9223f8862d80ac5d8599
Tomaž Erjavec <tomaz.erjavec@ijs.si>
2021-04-11 13:01:56 +0200

    More polishing.

--- Scripts/polish.pl
+++ Scripts/polish.pl
@@ -4,18 +4,14 @@ use warnings;
 use utf8;
 undef $/;
 $txt = <>;
-$txt =~ s|([^>]) *\n\s*|$1 |g; #join lines
+$txt =~ s|([^>])[ \t]*\n\s*|$1 |g; #join lines
 $txt =~ s|(<p [^>]*>)\s+|$1|g;
 $txt =~ s|(<p>)\s+|$1|g;
-#$txt =~ s|\n\s*<desc>|<desc>|g;
-#$txt =~ s|</desc>\n\s*|</desc>|g;
 $txt =~ s|\n\s*<term|<term|g;
 $txt =~ s|</term>\n\s*|</term>|g;
 $txt =~ s|</gap> (\S)|</gap>$1|g;
 $txt =~ s|\n\s*<w |\n<w |g;
 $txt =~ s|\n\s*<pc |\n<pc |g;
-$txt =~ s|\n\s*<s |\n<s |g;
-$txt =~ s|\n\s*<linkGrp |\n<linkGrp |g;
 $txt =~ s|\n\s*<link |\n<link |g;
 print $txt;
 print "\n";
