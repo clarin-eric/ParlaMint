13069c7a4e72aba610ceb8c2ff81b4a8a8561972
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2022-02-01 10:59:33 +0100

    add filesize testing (fail when total file size is > 100MB) run whole walidation process when only Scripts/Schema directories are changed

--- .github/actions/ParlaMintStatus/status.sh
+++ .github/actions/ParlaMintStatus/status.sh
@@ -2,13 +2,24 @@ pwd
 cd ParlaMint
 
 changed_files=$(git diff --name-only HEAD HEAD~1)
-parla_changed=$(echo "$changed_files"|grep 'ParlaMint-.*/'|sed 's/^ParlaMint-\([-A-Z]*\).*$/\1/'|sort|uniq|tr '\n' ' ')
-scripts_changed=$(echo "$changed_files"|grep -vc 'ParlaMint-.*')
+parla_changed=$(echo "$changed_files"|grep 'ParlaMint-.*/'|sed -n 's/^ParlaMint-\([-A-Z]*\).*.xml$/\1/p'|sort|uniq|tr '\n' ' '|sed 's/ *$//')
+scripts_changed=$(echo "$changed_files"|egrep  "^(Schema|Scripts)")
 parla_all=$(echo ParlaMint-*|sed 's/ParlaMint-\([-A-Z]*\)/\1/g'|sort)
 
 
 parla_process=$(test -z "${parla_changed}" && echo "${parla_all}" || echo "${parla_changed}")
 parla_process=$(echo "[\"$parla_process\"]"|sed 's/  */","/g'| sed 's/^\[""\]$/[]/;s/,""//')
+
+parla_changed_size=0
+for parla in $parla_changed;
+do
+  size=$(find ParlaMint-$parla -type f -name "ParlaMint-$parla*.xml"  -print0 | du -c --block-size=1000000 --files0-from=-|tail -1|cut -f 1)
+  echo "::notice:: ParlaMint-$parla size =${size} MB"
+  parla_changed_size=$(echo "$parla_changed_size+$size"|bc)
+done
+
+echo "::notice:: total changed parla tei files size=${parla_changed_size} MB"
+
 
 parla_changed=$(echo "[\"$parla_changed\"]"|sed 's/  */","/g'| sed 's/^\[""\]$/[]/;s/,""//')
 
@@ -26,3 +37,5 @@ echo "DEBUG: parla_process=${parla_process}"
 echo ::set-output name=parla_process::${parla_process}
 echo ::set-output name=parla_all::${parla_all}
 echo ::set-output name=parla_changed::${parla_changed}
+echo ::set-output name=scripts_changed::${scripts_changed}
+echo ::set-output name=parla_changed_size::${parla_changed_size}
