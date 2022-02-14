a59653f127710f72fd5106d01bd1ffc018c8bd46
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2022-02-14 11:14:59 +0100

    update paths in workflows

--- .github/actions/ParlaMintStatus/status.sh
+++ .github/actions/ParlaMintStatus/status.sh
@@ -2,7 +2,7 @@ pwd
 cd ParlaMint
 
 changed_files=$(git diff --name-only HEAD HEAD~1)
-parla_changed=$(echo "$changed_files"|grep 'ParlaMint-.*/'|sed -n 's/^ParlaMint-\([-A-Z]*\).*.xml$/\1/p'|sort|uniq|tr '\n' ' '|sed 's/ *$//')
+parla_changed=$(echo "$changed_files"|grep 'Data/ParlaMint-.*/'|sed -n 's/^Data/ParlaMint-\([-A-Z]*\).*.xml$/\1/p'|sort|uniq|tr '\n' ' '|sed 's/ *$//')
 scripts_changed=$(echo "$changed_files"|egrep  "^(Schema|Scripts)")
 parla_all=$(echo ParlaMint-*|sed 's/ParlaMint-\([-A-Z]*\)/\1/g'|sort)
 
@@ -13,7 +13,7 @@ parla_process=$(echo "[\"$parla_process\"]"|sed 's/  */","/g'| sed 's/^\[""\]$/[
 parla_changed_size=0
 for parla in $parla_changed;
 do
-  size=$(find ParlaMint-$parla -type f -name "ParlaMint-$parla*.xml"  -print0 | du -c --block-size=1000000 --files0-from=-|tail -1|cut -f 1)
+  size=$(find Data/ParlaMint-$parla -type f -name "ParlaMint-$parla*.xml"  -print0 | du -c --block-size=1000000 --files0-from=-|tail -1|cut -f 1)
   echo "::notice:: ParlaMint-$parla size =${size} MB"
   parla_changed_size=$(echo "$parla_changed_size+$size"|bc)
 done
